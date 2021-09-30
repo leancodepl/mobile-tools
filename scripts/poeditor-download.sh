cb0bfda86d0ada54aa1440604707c501f79fb518
Bartek Pacia <barpac02@gmail.com>
2021-09-30 18:18:29 +0200

    clean up code

--- scripts/poeditor-download.sh
+++ scripts/poeditor-download.sh
@@ -3,15 +3,13 @@ set -euo pipefail
 
 # Set the variables below according to your needs
 poeditorProjectId="355089"
-translationsDir="../assets/l10n/"
-stringsFile="../lib/l10n/strings.dart"
+translationsDir="./assets/l10n"
+stringsFile="./lib/l10n/strings.dart"
 mainLang="pl"
 langs=("pl") # ("pl" "en" "de") etc
 
-pushd "$(dirname $0)"
-
 mkdir -p $translationsDir
-mkdir -p "$(dirname $stringsFile)"
+mkdir -p $(dirname "$stringsFile")
 
 for lang in "${langs[@]}"; do
     url=$(curl -sS -X POST https://api.poeditor.com/v2/projects/export \
@@ -21,28 +19,27 @@ for lang in "${langs[@]}"; do
         -d type="key_value_json" \
     | jq '.result.url')
 
-    curl -sS "${url//\"}" -o "$translationsDir$lang.json"
+    curl -sS "${url//\"}" -o "$translationsDir/$lang.json"
 done
 
 # generate Strings class from JSON
 echo "// ignore_for_file: lines_longer_than_80_chars,constant_identifier_names" > $stringsFile
-echo "" >> $stringsFile
-echo "class Strings {" >> $stringsFile
+echo "" >> "$stringsFile"
+echo "class Strings {" >> "$stringsFile"
 
 # workaround for https://stackoverflow.com/a/47576101/7009800 
-HEAD="head"
-echo 'test' | head -n -1 &> /dev/null
-if [ "$?" -ne 0 ]; then
-    HEAD="ghead"
+if command -v ghead >& /dev/null
+then
+    head=ghead
+else
+    head=head
 fi
 
-jq 'keys' "$translationsDir$mainLang.json" \
-| $HEAD -n -1 \
+jq 'keys' "$translationsDir/$mainLang.json" \
+| $head -n -1 \
 | tail -n +2 \
 | awk -F'"' '{ name = $2; gsub(/\./, "_", name); printf "    static const %s = '"'"'%s'"'"';\n", name, $2 }' \
->> $stringsFile
+>> "$stringsFile"
 
 
-echo "}" >> $stringsFile
-
-popd
+echo "}" >> "$stringsFile"
