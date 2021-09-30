c3c8213f1bb733cc13b171c1b80919700a443b08
Bartek Pacia <barpac02@gmail.com>
2021-09-30 11:18:40 +0200

    remove redundant comments

--- scripts/generate-keystores.sh
+++ scripts/generate-keystores.sh
@@ -5,14 +5,14 @@ IFS=$'\n\t'
 # adapted from: https://leancode.atlassian.net/wiki/spaces/LEAN/pages/1293844508/Android+signing+policy
 
 app_name="$1"
-owner="$2" # used for prod and prod_upload
+owner="$2"
 
 echo "Creating keystore for app $app_name with owner $owner"
 
 gen_key() {
     app_name="$1"
-    owner="$2" # default: LeanCode
-    flavor="$3" # {tst, prod, prod_upload}
+    owner="$2"
+    flavor="$3"
     
     echo "Generating $flavor key for $app_name:"
     
